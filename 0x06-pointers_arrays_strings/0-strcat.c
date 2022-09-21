defa5e61c0b924ed89a8e037dd0828c5bff19595
Atoberdygit <worms12340@gmail.com>
2022-09-21 14:02:37 -0700

    fix str error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,50 +1,44 @@
 #include "main.h"
 
+
 /**
-
- * _strcat - concatenates two strings,
-
- * @dest: destination.
-
- * @src: source.
-
- * Return: the pointer to dest.
-
+ *_strcat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
  */
 
-char *_strcat(char *dest, char *src)
 
+char *_strcat(char *dest, char *src)
 {
 
-	int count = 0, count2 = 0;
+	int i = 0, j = 0;
 
 
 
-	while (*(dest + count) != '\0')
+	while (dest[i] != '\0')
+
+		i++;
+
+
+
+	while (src[j] != '\0')
 
 	{
 
-		count++;
+		dest[i] = src[j];
+
+		i++;
+
+		j++;
 
 	}
 
 
 
-	while (count2 >= 0)
+	dest[i] = '\0';
 
-	{
 
-		*(dest + count) = *(src + count2);
-
-		if (*(src + count2) == '\0')
-
-			break;
-
-		count++;
-
-		count2++;
-
-	}
 
 	return (dest);
 
